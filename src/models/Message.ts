a84fce90fd07856aab48b648112a2f06c1224d8b
sarashikha <114863116+sarashikha@users.noreply.github.com>
2025-12-28 03:32:56 +0200

    Format Message interface for better readability

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -1,1 +1,6 @@
-export interface Message { id: string; title: string; content: string; date: string }
+export interface Message {
+  id: string;
+  title: string;
+  content: string;
+  date: string;
+}
