860926201867f10961613847fbea5639d1a7b3fe
sarashikha <114863116+sarashikha@users.noreply.github.com>
2025-12-28 03:37:56 +0200

    Refactor App component layout with Box wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import Header from './components/layout/Header';
 import DrawerMenu from './components/layout/DrawerMenu';
 import AppRoutes from './routes/AppRoutes';
+import { Box } from '@mui/material';
 
 export default function App() {
   return (
     <>
       <Header />
-      <DrawerMenu />
-      <AppRoutes />
+      <Box sx={{ display: 'flex' }}>
+        <DrawerMenu />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <AppRoutes />
+        </Box>
+      </Box>
     </>
   );
 }
-
