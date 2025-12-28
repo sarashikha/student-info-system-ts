b2682fd1ed96926e487946e88fdb9e2088715ad6
sarashikha <114863116+sarashikha@users.noreply.github.com>
2025-12-28 03:35:13 +0200

    Update AdminHome.tsx

--- src/pages/admin/AdminHome.tsx
+++ src/pages/admin/AdminHome.tsx
@@ -1,1 +1,11 @@
-export default function AdminHome(){return <div>Admin Home</div>}
+import { Typography, Stack } from '@mui/material';
+
+export default function AdminHome() {
+  return (
+    <Stack spacing={2}>
+      <Typography variant="h4">מסך בית מנהל</Typography>
+      <Typography>ניהול קורסים, סטודנטים והודעות</Typography>
+    </Stack>
+  );
+}
+
