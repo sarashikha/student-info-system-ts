6ea12a84e3fff796adb422da6a97b874563a8e3a
sarashikha <114863116+sarashikha@users.noreply.github.com>
2025-12-28 03:05:11 +0200

    Refactor storageService and add init method
    Refactored storageService methods and added initialization for degree requirements.

--- src/services/storageService.ts
+++ src/services/storageService.ts
@@ -1,23 +1,17 @@
+import { initialDegreeReqs } from '../data/initialData';
+
 export const storageService = {
   get<T>(key: string): T[] {
     return JSON.parse(localStorage.getItem(key) || '[]');
   },
 
-  save<T>(key: string, data: T[]) {
+  set<T>(key: string, data: T[]) {
     localStorage.setItem(key, JSON.stringify(data));
   },
 
-  add<T>(key: string, item: T) {
-    const data = this.get<T>(key);
-    data.push(item);
-    this.save(key, data);
-  },
-
-  update<T extends { id: string }>(key: string, item: T) {
-    const data = this.get<T>(key).map((i: any) =>
-      i.id === item.id ? item : i
-    );
-    this.save(key, data);
+  init() {
+    if (!localStorage.getItem('degreeReqs')) {
+      localStorage.setItem('degreeReqs', JSON.stringify(initialDegreeReqs));
+    }
   }
 };
-
