bb0f2c2c092d6a672ee2482f1d85775c60fde2ad
Noor <lightsun16@yaoo.com>
2026-02-15 22:06:01 +0200

    Add all project files excluding ignored files

---
+++ App.jsx
@@ -0,0 +1,19 @@
+// src/App.jsx
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import { AuthProvider } from "./context/AuthContext";
+import AppRoutes from "./routes/AppRoutes";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ThemeProvider>
+        <AuthProvider>
+          <AppRoutes />
+        </AuthProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
